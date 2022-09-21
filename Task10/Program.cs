4ad1ccf2eb294087d0263f31a27c7a26423d48c0
ivan <i.semenychbk.ru@gmail.com>
2022-09-21 23:58:33 +0300

    finish10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,1 +1,10 @@
 ﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого число
+
+using static System.Console;
+
+Clear();
+Write("Введите трехзначное число: ");
+int number=int.Parse(ReadLine()!);
+int n2 = number/100;
+int n1 = (number - n2*100)/10;
+WriteLine($"result = {n1}");
